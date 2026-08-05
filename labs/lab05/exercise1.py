9649bdecbdf6184de20f68dd6848063ace763b6f
daniaareanna08-beep <daniaareanna08@gmail.com>
2026-08-05 12:15:08 +0800

    lab05 exercise1

---
+++ labs/lab05/exercise1.py
@@ -0,0 +1,13 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+course = input("Enter your course code:")
+
+print("Student Information")
+print("Name:",name)
+print("Age:",age)
+print("Course:",course)
+
+print("Data Types")
+print(type(name))
+print(type(age))
+print(type(course))
